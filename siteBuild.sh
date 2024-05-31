6f9d47fd84b83a1440729983e728bc4ea9105d6d
jos on devone laptop <jneuhaus@locatortechnologies.com>
2024-05-31 09:54:05 -0400

    Upgraded Astro

--- siteBuild.sh
+++ siteBuild.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-readonly BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" || exit; pwd)"
+readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" || exit; pwd)"
 
-rm -r ${BASEDIR}/dist && mkdir -p ${BASEDIR}/dist
+rm -r ${SCRIPT_DIR}/dist && mkdir -p ${SCRIPT_DIR}/dist
 
 if [[ $(command -v bun) ]]; then
-	bun run build && cd ${BASEDIR}/dist && http-server && cd ${BASEDIR}
+	bun run build && cd ${SCRIPT_DIR}/dist && http-server && cd ${SCRIPT_DIR}
 else
-	npm run build && cd ${BASEDIR}/dist && http-server && cd ${BASEDIR}
+	npm run build && cd ${SCRIPT_DIR}/dist && http-server && cd ${SCRIPT_DIR}
 fi
---
+++ siteUpgrade.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" || exit; pwd)"
+
+rm -r ${SCRIPT_DIR}/dist && mkdir -p ${SCRIPT_DIR}/dist
+
+if [[ $(command -v bunx) ]]; then
+	bunx @astrojs/upgrade
+else
+	npx @astrojs/upgrade
+fi
