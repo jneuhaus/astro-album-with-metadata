70a618ae87ad54a6a85a49e03cff90771c2b0f86
jos on devone laptop <jneuhaus@locatortechnologies.com>
2025-01-12 16:10:21 -0500

    Upgraded Astro

--- siteUpgrade.sh
+++ siteUpgrade.sh
@@ -4,8 +4,8 @@ readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" || exit; pwd)"
 
 rm -r ${SCRIPT_DIR}/dist && mkdir -p ${SCRIPT_DIR}/dist
 
-if [[ $(command -v bunx) ]]; then
-	bunx @astrojs/upgrade
+if [[ $(command -v deno) ]]; then
+	deno run -A npm:@astrojs/upgrade
 else
 	npx @astrojs/upgrade
 fi
